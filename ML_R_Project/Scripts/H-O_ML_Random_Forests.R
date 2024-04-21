0e81a6dc82fe22700a5222612dd583bf0c736e77
cgriffin480 <166812914+cgriffin480@users.noreply.github.com>
2024-04-21 18:00:44 +0200

    Tutorials for Random Forests

---
+++ ML_R_Project/Scripts/H-O_ML_Random_Forests.R
@@ -0,0 +1,246 @@
+#--------------------------- Title ---------------------------------------------
+  
+  ## Project: ML_KR_Project.Rproj
+  ## Script:  H-O_ML_Random_Forests.R
+  
+  ## Programmer: Christopher Griffin
+  ## Creation Date: 4/21/2024
+  ## Edit Date:  4/21/2024
+  ## Notes: This is the tutorial for random forests from Hands-on Machine 
+  ## Learning with R
+  
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# Chapter 11 Random Forests
+# 
+# Random forests are a modification of bagged decision trees that build a large 
+# collection of de-correlated trees to further improve predictive performance. 
+# They have become a very popular “out-of-the-box” or “off-the-shelf” learning 
+# algorithm that enjoys good predictive performance with relatively little hyperparameter tuning.
+
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+
+# 11.1 Prerequisites
+# 
+# This chapter leverages the following packages. Some of these packages play a 
+# supporting role; however, the emphasis is on how to implement random forests 
+# with the ranger (Wright and Ziegler 2017) and h2o packages.
+
+#-------------------------------------------------------------------------------
+
+# Helper packages
+
+library(dplyr)    # for data wrangling
+library(ggplot2)  # for awesome graphics
+
+
+# install the following packages:
+# install.packages("ranger")
+# install.packages("h2o")
+# install.packages("tidymodels")
+
+# Modeling packages
+library(ranger)   # a c++ implementation of random forest 
+library(h2o)      # a java-based implementation of random forest
+library(tidymodels) # for the ames housing dataset.
+
+
+#-------------------------------------------------------------------------------
+
+# The basic algorithm for a regression or classification random forest can be 
+# generalized as follows:
+#   
+#   1.  Given a training data set
+#   2.  Select number of trees to build (n_trees)
+#   3.  for i = 1 to n_trees do
+#   4.  |  Generate a bootstrap sample of the original data
+#   5.  |  Grow a regression/classification tree to the bootstrapped data
+#   6.  |  for each split do
+#   7.  |  | Select m_try variables at random from all p variables
+#   8.  |  | Pick the best variable/split-point among the m_try
+#   9.  |  | Split the node into two child nodes
+#   10. |  end
+#   11. | Use typical tree model stopping criteria to determine when a 
+#         tree is complete (but do not prune)
+#   12. end
+#   13. Output ensemble of trees 
+
+#-------------------------------------------------------------------------------
+
+# We’ll continue working with the ames_train data set created in Section 2.7 to illustrate the main concepts.
+
+# Stratified sampling with the rsample package
+set.seed(123)
+split <- initial_split(ames, prop = 0.7, 
+                       strata = "Sale_Price")
+ames_train  <- training(split)
+ames_test   <- testing(split)
+
+# number of features
+n_features <- length(setdiff(names(ames_train), "Sale_Price"))
+
+# train a default random forest model
+ames_rf1 <- ranger(
+  Sale_Price ~ .,
+  data = ames_train,
+  mtry = floor(n_features / 3),
+  respect.unordered.factors = "order",
+  seed = 123
+)
+
+# get OOB RMSE
+(default_rmse <- sqrt(ames_rf1$prediction.error))
+
+#-------------------------------------------------------------------------------
+
+# 11.4 Hyperparameters
+
+# Although random forests perform well out-of-the-box, there are several tunable 
+# hyperparameters that we should consider when training a model. The main 
+# hyperparameters to consider include:
+#   
+# 1. The number of trees in the forest
+# 2. The number of features to consider at any given split: mtry
+# 3. The complexity of each tree
+# 4. The sampling scheme
+# 5. The splitting rule to use during tree construction
+# 
+# and (2) typically have the largest impact on predictive accuracy and should always
+# be tuned. (3) and (4) tend to have marginal impact on predictive accuracy but 
+# are still worth exploring. They also have the ability to influence computational 
+# efficiency. (5) tends to have the smallest impact on predictive accuracy and is 
+# used primarily to increase computational efficiency.
+
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 11.4.1 Number of trees
+# The first consideration is the number of trees within your random forest. 
+# Although not technically a hyperparameter, the number of trees needs to be 
+# sufficiently large to stabilize the error rate. A good rule of thumb is to 
+# start with 10 times the number of features; however, as you adjust other 
+# hyperparameters such as mtry and node size, more or fewer trees may be required. 
+# More trees provide more robust and stable error estimates and variable importance 
+# measures; however, the impact on computation time increases linearly with the 
+# number of trees.
+
+#-------------------------------------------------------------------------------
+
+# create hyperparameter grid
+1/(sqrt(n_features))
+
+hyper_grid <- expand.grid(
+  mtry = floor(n_features * c(.05, .117, .15, .25, .333, .4)),
+  min.node.size = c(1, 3, 5, 10), 
+  replace = c(TRUE, FALSE),                               
+  sample.fraction = c(.5, .63, .8),                       
+  rmse = NA                                               
+)
+
+# execute full cartesian grid search
+for(i in seq_len(nrow(hyper_grid))) {
+  # fit model for ith hyperparameter combination
+  fit <- ranger(
+    formula         = Sale_Price ~ ., 
+    data            = ames_train, 
+    num.trees       = n_features * 10,
+    mtry            = hyper_grid$mtry[i],
+    min.node.size   = hyper_grid$min.node.size[i],
+    replace         = hyper_grid$replace[i],
+    sample.fraction = hyper_grid$sample.fraction[i],
+    verbose         = FALSE,
+    seed            = 123,
+    respect.unordered.factors = 'order',
+  )
+  # export OOB error 
+  hyper_grid$rmse[i] <- sqrt(fit$prediction.error)
+}
+
+# assess top 10 models
+hyper_grid %>%
+  arrange(rmse) %>%
+  mutate(perc_gain = (default_rmse - rmse) / default_rmse * 100) %>%
+  head(10)
+
+#-------------------------------------------------------------------------------
+
+# using h2o to randomly search for an optimal model rather than an exhausting cartesian search.
+
+#-------------------------------------------------------------------------------
+
+# To fit a random forest model with h2o, we first need to initiate our h2o session.
+h2o.no_progress()
+h2o.init(max_mem_size = "5g")
+
+# Next, we need to convert our training and test data sets to objects that h2o can work with.
+
+# convert training data to h2o object
+train_h2o <- as.h2o(ames_train)
+
+# set the response column to Sale_Price
+response <- "Sale_Price"
+
+# set the predictor names
+predictors <- setdiff(colnames(ames_train), response)
+
+# The following fits a default random forest model with h2o to illustrate that our baseline results ( 
+#   OOB RMSE = 24439) are very similar to the baseline ranger model we fit earlier.
+
+h2o_rf1 <- h2o.randomForest(
+  x = predictors, 
+  y = response,
+  training_frame = train_h2o, 
+  ntrees = n_features * 10,
+  seed = 123
+)
+
+h2o_rf1
+
+
+# hyperparameter grid
+hyper_grid <- list(
+  mtries = floor(n_features * c(.05, .15, .25, .333, .4)),
+  min_rows = c(1, 3, 5, 10),
+  max_depth = c(10, 20, 30),
+  sample_rate = c(.55, .632, .70, .80)
+)
+
+# random grid search strategy
+search_criteria <- list(
+  strategy = "RandomDiscrete",
+  stopping_metric = "mse",
+  stopping_tolerance = 0.001,   # stop if improvement is < 0.1%
+  stopping_rounds = 10,         # over the last 10 models
+  max_runtime_secs = 60*5      # or stop search after 5 min.
+)
+
+# perform grid search 
+random_grid <- h2o.grid(
+  algorithm = "randomForest",
+  grid_id = "rf_random_grid",
+  x = predictors, 
+  y = response, 
+  training_frame = train_h2o,
+  hyper_params = hyper_grid,
+  ntrees = n_features * 10,
+  seed = 123,
+  stopping_metric = "RMSE",   
+  stopping_rounds = 10,           # stop if last 10 trees added 
+  stopping_tolerance = 0.005,     # don't improve RMSE by 0.5%
+  search_criteria = search_criteria
+)
+
+# collect the results and sort by our model performance metric 
+# of choice
+random_grid_perf <- h2o.getGrid(
+  grid_id = "rf_random_grid", 
+  sort_by = "mse", 
+  decreasing = FALSE
+)
+random_grid_perf
--- ML_R_Project/Scripts/Trees_1.R
+++ ML_R_Project/Scripts/Trees_1.R
@@ -48,3 +48,234 @@ plot(tree.carseats)
 text(tree.carseats, pretty = 0)
 
 tree.carseats
+
+#-------------------------------------------------------------------------------
+
+  ## In order to properly evaluate the performance of a classification tree on
+  ## these data, we must estimate the test error rather than simply computing
+  ## the training error. We split the observations into a training set and a test
+  ## set, build the tree using the training set, and evaluate its performance on the
+  ## test data.
+
+#-------------------------------------------------------------------------------
+
+
+set.seed(2) # for reproducability.
+
+train <- sample(1:nrow(Carseats), 200)
+
+Carseats.test <- Carseats[-train, ]
+
+High.test <- High[-train]
+
+tree.carseats <- tree(High ~ . - Sales, Carseats, subset = train)
+
+tree.pred <- predict(tree.carseats, Carseats.test, type = "class")
+
+table(tree.pred, High.test)
+
+(104 + 50) / 200
+
+#-------------------------------------------------------------------------------
+
+# Next, we consider whether pruning the tree might lead to improved results. The function
+# cv.tree() performs cross-validation in order to 
+# determine the optimal level of tree complexity; cost complexity pruning
+# is used in order to select a sequence of trees for consideration. We use
+# the argument FUN = prune.misclass in order to indicate that we want the 
+# classification error rate to guide the cross-validation and pruning process,
+
+#-------------------------------------------------------------------------------
+
+set.seed(7)
+
+cv.carseats <- cv.tree(tree.carseats, FUN = prune.misclass)
+names(cv.carseats)
+
+cv.carseats
+
+par(mfrow = c(1,2))
+plot(cv.carseats$size, cv.carseats$dev, type = "b")
+plot(cv.carseats$k, cv.carseats$dev, type = "b")
+
+prune.carseats <- prune.misclass(tree.carseats, best = 9)
+
+par(mfrow = c(1,1))
+plot(prune.carseats)
+text(prune.carseats, pretty = 0)
+
+tree.pred <- predict(prune.carseats, Carseats.test, type = "class")
+table(tree.pred, High.test)
+(97 + 58) / 200
+
+
+#-------------------------------------------------------------------------------
+
+# 8.3.2 Fitting Regression Trees
+
+# Here we fit a regression tree to the Boston data set. First, we create a
+# training set, and fit the tree to the training data.
+
+#-------------------------------------------------------------------------------
+
+?Boston
+
+summary(Boston)
+
+set.seed(1)
+
+train <- sample(1:nrow(Boston), nrow(Boston) / 2)
+
+tree.boston <- tree(medv ~ ., Boston, subset = train)
+
+summary(tree.boston)
+
+plot(tree.boston)
+text(tree.boston, pretty = 0)
+
+# using cross validation to prune.
+
+cv.boston <- cv.tree(tree.boston)
+plot(cv.boston$size, cv.boston$dev, type = "b")
+
+
+yhat <- predict(tree.boston, newdata = Boston[-train, ])
+boston.test <- Boston[-train, "medv"]
+plot(yhat, boston.test)
+abline(0,1)
+mean((yhat - boston.test)^2)
+
+#-------------------------------------------------------------------------------
+
+# 8.3.3 Bagging and Random Forests
+# Here we apply bagging and random forests to the Boston data, using the
+# randomForest package in R. The exact results obtained in this section may
+# depend on the version of R and the version of the randomForest package
+# installed on your computer. Recall that bagging is simply a special case of
+# a random forest with m = p. Therefore, the randomForest() function can be used to perform both random forests and bagging.
+
+#-------------------------------------------------------------------------------
+
+
+# install.packages("randomForest")
+
+library(randomForest)
+
+set.seed(1)
+
+bag.boston <- randomForest(medv ~ ., data = Boston,
+                           subset = train, mtry = 12, importance = TRUE)
+bag.boston
+
+
+yhat.bag <- predict(bag.boston, newdata = Boston[-train, ])
+
+plot(yhat.bag, boston.test)
+abline(0,1)
+mean((yhat.bag - boston.test)^2)
+
+#-------------------------------------------------------------------------------
+
+# Growing a random forest proceeds in exactly the same way, except that
+# we use a smaller value of the
+# argument. By default,
+# mtry
+# randomForest()
+# uses
+# variables when building a random forest of regression trees, and
+# p/3
+# 
+# variables when building a random forest of classification trees.
+# vp
+
+#-------------------------------------------------------------------------------
+
+set.seed(1)
+
+rf.boston <- randomForest(medv ~., data = Boston,
+                          subset = train, mtry = 6, importance = TRUE)
+
+yhat.rf <- predict(rf.boston, newdata = Boston[-train, ])
+
+mean((yhat.rf - boston.test)^2)
+
+importance(rf.boston)
+
+varImpPlot(rf.boston)
+
+# The results indicate that across all of the trees considered in the random
+# forest, the wealth of the community (lstat) and the house size (rm) are by
+# far the two most important variables.
+
+#-------------------------------------------------------------------------------
+
+  ## example code provided in function documentation
+
+#-------------------------------------------------------------------------------
+
+## Classification:
+##data(iris)
+set.seed(71)
+iris.rf <- randomForest(Species ~ ., data=iris, importance=TRUE,
+                        proximity=TRUE)
+print(iris.rf)
+## Look at variable importance:
+round(importance(iris.rf), 2)
+## Do MDS on 1 - proximity:
+iris.mds <- cmdscale(1 - iris.rf$proximity, eig=TRUE)
+op <- par(pty="s")
+pairs(cbind(iris[,1:4], iris.mds$points), cex=0.6, gap=0,
+      col=c("red", "green", "blue")[as.numeric(iris$Species)],
+      main="Iris Data: Predictors and MDS of Proximity Based on RandomForest")
+par(op)
+print(iris.mds$GOF)
+
+## The `unsupervised' case:
+set.seed(17)
+iris.urf <- randomForest(iris[, -5])
+MDSplot(iris.urf, iris$Species)
+
+## stratified sampling: draw 20, 30, and 20 of the species to grow each tree.
+(iris.rf2 <- randomForest(iris[1:4], iris$Species, 
+                          sampsize=c(20, 30, 20)))
+
+## Regression:
+## data(airquality)
+set.seed(131)
+ozone.rf <- randomForest(Ozone ~ ., data=airquality, mtry=3,
+                         importance=TRUE, na.action=na.omit)
+print(ozone.rf)
+## Show "importance" of variables: higher value mean more important:
+round(importance(ozone.rf), 2)
+
+## "x" can be a matrix instead of a data frame:
+set.seed(17)
+x <- matrix(runif(5e2), 100)
+y <- gl(2, 50)
+(myrf <- randomForest(x, y))
+(predict(myrf, x))
+
+## "complicated" formula:
+(swiss.rf <- randomForest(sqrt(Fertility) ~ . - Catholic + I(Catholic < 50),
+                          data=swiss))
+(predict(swiss.rf, swiss))
+## Test use of 32-level factor as a predictor:
+set.seed(1)
+x <- data.frame(x1=gl(53, 10), x2=runif(530), y=rnorm(530))
+(rf1 <- randomForest(x[-3], x[[3]], ntree=10))
+
+## Grow no more than 4 nodes per tree:
+(treesize(randomForest(Species ~ ., data=iris, maxnodes=4, ntree=30)))
+
+## test proximity in regression
+iris.rrf <- randomForest(iris[-1], iris[[1]], ntree=101, proximity=TRUE, oob.prox=FALSE)
+str(iris.rrf$proximity)
+
+## Using weights: make versicolors having 3 times larger weights
+iris_wt <- ifelse( iris$Species == "versicolor", 3, 1 )
+set.seed(15)
+iris.wcrf <- randomForest(iris[-5], iris[[5]], weights=iris_wt, keep.inbag=TRUE)
+print(rowSums(iris.wcrf$inbag))
+set.seed(15)
+iris.wrrf <- randomForest(iris[-1], iris[[1]], weights=iris_wt, keep.inbag=TRUE)
+print(rowSums(iris.wrrf$inbag))
